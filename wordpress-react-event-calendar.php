f89f9af84e6160f635cd7749c9f916279e7da37e
Alex Minkoff <minkoff@gmail.com>
2018-02-18 05:35:53 +0000

    Add plugin header

---
+++ wordpress-react-event-calendar.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:  WordPress React Event Calendar
+Plugin URI:   http://alexminkoff.com/
+Description:  Event Calendar for WordPress, built with React
+Version:      20180218
+Author:       Alex Minkoff
+Author URI:   http://alexminkoff.com/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wordpress-react-event-calendar
+*/
