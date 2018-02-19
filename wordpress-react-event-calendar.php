c329a2832b085d31f744fd6ad1e320f8f7b152ec
Alex Minkoff <minkoff@gmail.com>
2018-02-19 23:28:45 +0000

    Add calendar shortcode

--- wordpress-react-event-calendar.php
+++ wordpress-react-event-calendar.php
@@ -11,26 +11,53 @@ License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  wordpress-react-event-calendar
 */
 
-if ( !defined( 'WPINC' ) )
+if ( ! defined( 'WPINC' ) )
 	die;
 
 define( 'WPREC_VERSION', '1.0.0' );
 
 class WordPress_React_Event_Calendar
 {
+	/*
+	Constructor
+	*/
 	public function __construct() {
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'public_scripts' ), 10 );
+		add_action( 'init', array( $this, 'register_shortcode' ) );
 	}
 
-	public function admin_scripts() {
-		wp_enqueue_style( 'wprec-admin', plugins_url('assets/admin.css', __FILE__), array(), WPREC_VERSION, 'all' );
-		wp_enqueue_script( 'wprec-admin', plugins_url('assets/admin.js', __FILE__), array(), WPREC_VERSION );
-	}
-
+	/*
+	Enqueue scripts and styles
+	*/
 	public function public_scripts() {
 		wp_enqueue_style( 'wprec-public', plugins_url( 'assets/public.css', __FILE__ ), array(), WPREC_VERSION, 'all' );
-		wp_enqueue_script( 'wprec-public', plugins_url('assets/public.js', __FILE__), array(), WPREC_VERSION );
+		wp_enqueue_script( 'wprec-public', plugins_url( 'assets/public.js' , __FILE__ ), array(), WPREC_VERSION );
+	}
+
+	/*
+	Enqueue admin scripts and styles
+	*/
+	public function admin_scripts() {
+		wp_enqueue_style( 'wprec-admin', plugins_url( 'assets/admin.css', __FILE__ ), array(), WPREC_VERSION, 'all' );
+		wp_enqueue_script( 'wprec-admin', plugins_url( 'assets/admin.js', __FILE__ ), array(), WPREC_VERSION );
+	}
+
+	/*
+	Register calendar shortcode
+	*/
+	public function register_shortcode() {
+		add_shortcode( 'wprec', array( $this, 'shortcode' ) );
+	}
+
+	/*
+	Display calendar
+	*/
+	function shortcode( $atts = [], $content = null, $tag = '' ) {
+		if (isset($atts['id'])) {
+			return '<div class="wprec-calendar" data-id="' . $atts['id'] . '"></div>';
+		}
+		return '';
 	}
 }
 
